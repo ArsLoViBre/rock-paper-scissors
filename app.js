c5b766c84e68fdafcccb0fe5ef18d9249c365517
Arsen Tuniev <arsen.tuniev.study@gmail.com>
2024-05-28 21:49:07 +0200

    Add logic to get human choice

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function getComputerChoice(){
     let choice
-    num = Math.floor(Math.random() * 3)
+    let num = Math.floor(Math.random() * 3)
     switch (num) {
         case 0:
             choice = 'rock';
@@ -18,3 +18,24 @@ function getComputerChoice(){
 }
 
 console.log(getComputerChoice())
+
+function getHumanChoice(){
+    let choice
+    let num = parseInt(prompt("Make a choice:\n 0: Rock\n 1: Paper\n 2: Scissors"))
+    switch (num) {
+        case 0:
+            choice = 'rock';
+            break
+        case 1:
+            choice = 'paper';
+            break
+        case 2:
+            choice = 'scissors';
+            break
+        default:
+            throw new Error("Something went wrong with getHumanChoice function");
+    }
+    return choice
+}
+
+console.log(getHumanChoice())
